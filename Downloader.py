c749b3a8660a5a85531b507d1c8368e2113433ab
Chris Sardegna <github@reagentx.net>
2017-05-22 22:04:45 -0700

    Rewrote script
    - Now works with new HLTV design
    - Script now gets the Match IDs, converts to Demo IDs, and downloads
    the demos
    - Calculates file sizes and total data transferred
    - Error trap for matches with no demo
    - Split repetitive URL code to a separate method

--- Downloader.py
+++ Downloader.py
@@ -4,74 +4,107 @@ import re
 import os
 
 
-def getIDs(eventID):
+def getMatchIDs(eventID):
     # Get the variable passed as an argument so it can be used
     eventid = eventID
     # Create an offset varibale for lists that are paginated on HLTV
     offset = 0
-    #  Build the URL
-    url = 'http://www.hltv.org/?pageid=28&&eventid=%s&offset=%s' % (eventid, offset)
-    # Open the URL
-    opener = urllib2.build_opener()
-    # Spoof the user agent
-    opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
-    response = opener.open(url)
-    # Read the response as HTML
-    html = response.read()
+    # Build the URL
+    url = 'https://www.hltv.org/results?offset=%s&event=%s' % (offset, eventid)
+    # Get the HTML using getHTML()
+    html = getHTML(url)
     # Create an array of all of the Demo URLs on the page
-    demoIDs = re.findall('"(.*?eventid=%s&offset=%d&amp;demoid=.*?)"' % (eventid, offset), html)
+    matchIDs = re.findall('"(.*?000"><a href="/matches/.*?)"', html)
     # Loops trhrough the messy array and removes the pesky parts
-    for i in range(0, len(demoIDs)):
-        demoIDs[i] = demoIDs[i].replace('" href="?pageid=28&amp;&eventid=%s&offset=%s&amp;demoid=' % (eventid, offset), "")
+    for i in range(0, len(matchIDs)):
+        matchIDs[i] = matchIDs[i].split('/', 2)[-1]
     # If the length is = 25, offset by 25 and loop again
-    if len(demoIDs) == 25:
-        # Set a boolean to cloe the while loop and a page variable we can increment when paginating
+    if len(matchIDs) == 50:
+        print "Parsed first page. Found %s IDs" % (len(matchIDs))
+        # Set a boolean to close the while loop and a page variable we can increment when paginating
         morePages = True
         page = 1
-        # While check is true, offset by 25
+        # While check is true, offset by 50
         while morePages:
-            offset += 25
+            offset += 50
             # Same URL building and parsing as above
-            url = 'http://www.hltv.org/?pageid=28&&eventid=%s&offset=%s' % (eventid, offset)
-            opener = urllib2.build_opener()
-            opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
-            response = opener.open(url)
-            html = response.read()
-            moreDemoIDs = re.findall('"(.*?eventid=%s&offset=%d&amp;demoid=.*?)"' % (eventid, offset), html)
-            for i in range(0, len(moreDemoIDs)):
-                moreDemoIDs[i] = moreDemoIDs[i].replace('" href="?pageid=28&amp;&eventid=%s&offset=%s&amp;demoid=' % (eventid, offset), "")
+            url = 'https://www.hltv.org/results?offset=%s&event=%s' % (offset, eventid)
+            html = getHTML(url)
+            moreMatchIDs = re.findall('"(.*?000"><a href="/matches/.*?)"', html)
+            for i in range(0, len(moreMatchIDs)):
+                moreMatchIDs[i] = moreMatchIDs[i].split('/', 2)[-1]
                 # Appends the new IDs to the master list
-                demoIDs.append(moreDemoIDs[i])
+                matchIDs.append(moreMatchIDs[i])
             # Determine if there are additional page to be found, if not the while loop ends
-            if len(moreDemoIDs) < 25:
+            if len(moreMatchIDs) < 50:
                 morePages = False
-                print "Parsing final page. Found %s IDs" % (len(demoIDs))
+                page += 1
+                print "Parsed page %s. Found %s IDs." % (page, len(matchIDs))
             else:
                 # Prints the current page and the number of parsed IDs
                 page += 1
-                print "Parsing next page: %s. %s IDs so far." % (page, len(demoIDs))
-    elif len(demoIDs) < 25:
-        print "Total demos: %s" % len(demoIDs)
-    elif len(demoIDs) > 25:
-        print "HLTV altered demo page layout"
+                print "Parsed page %s. %s IDs found so far." % (page, len(matchIDs))
+    elif len(matchIDs) < 50:
+        print "Total demos: %s" % len(matchIDs)
+    elif len(matchIDs) > 50:
+        print "HLTV altered demo page layout :("
+    return matchIDs
+
+
+def getDemoIDs(matchIDs):
+    # Get the variable passed as an argument so it can be used
+    matchIDs = matchIDs
+    # Tell ths user what is happening
+    print "Converting Match IDs to Demo IDs"
+    # Define the array of Demo IDs
+    demoIDs = []
+    # Create an array for Match IDs with no Demo ID
+    noDemos = []
+    # Create counter variable
+    counter = 0
+    # Loops through the array of Match IDs and gets the respective Demo IDs
+    for i in range(0, len(matchIDs)):
+        # Same URL building and opening as above
+        url = "https://www.hltv.org/matches/%s" % (matchIDs[i])
+        html = getHTML(url)
+        demoID = re.findall('"(/download/demo/.*?)"', html)
+        # Check if re.findall()'s array is empty
+        # If it has an element, add that Demo ID to the demoIDs array
+        if len(demoID) > 0:
+            demoIDs.append(demoID[0])
+        # If there is no element, print which match has no demo
+        elif len(demoID) < 1:
+            print "No demo found for %s (%s)" % (len(matchIDs)-counter, matchIDs[i].rsplit('/', 1)[-1])
+            noDemos.append(matchIDs[i])
+        # Update counter and tell the user the conversion status is
+        counter += 1
+        print "%s remaining to convert." % (len(matchIDs)-counter)
+    # Loop through the demoIDs array and remove everything up to the last / to get the real Demo ID
+    for i in range(0, len(demoIDs)):
+        demoIDs[i] = demoIDs[i].rsplit('/', 1)[-1]
+    # If there are errors, print them
+    printErrors(noDemos)
     return demoIDs
 
 
 def download(demoIDs):
     # Get the variable passed as an argument so it can be used
     demoIDs = demoIDs
+    # Tell the user how many demo files will be downloaded
+    print "%s demo files to retrieve." % (len(demoIDs))
     # Create a counter varibale
     counter = 0
-    # Make a folder for the files to be stored in.
-    eventName = raw_input("What is the event name? ")
+    # TODO Make a folder for the files to be stored in.
+    eventName = "Test Event"
+    # eventName = raw_input("What is the event name? ")
     directory = "./%s" % (eventName)
     os.mkdir(directory)
-    # Tell the user we are downloading the demo files
-    print "Now downloading demo files..."
+    # Create an array to calculate the total data transferred
+    filesizes = []
     # Parse through the array of Demo IDs
     for i in range(0, len(demoIDs)):
-        # Build the URL, same as above
-        url = "http://www.hltv.org/interfaces/download.php?demoid=%s" % (demoIDs[i])
+        # Build and open the URL
+        url = "https://www.hltv.org/download/demo/%s" % (demoIDs[i])
         opener = urllib2.build_opener()
         opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
         response = opener.open(url)
@@ -79,14 +112,73 @@ def download(demoIDs):
         finalurl = response.geturl()
         # Gets the filename (everything after the last trailing /)
         filename = finalurl.rsplit('/', 1)[-1]
-        # Downloads the file to the directory with the script
-        urllib.urlretrieve(finalurl, directory+"/"+filename)
+        # Gets the Content-Length from the metadata from finalurl
+        filesize = (int(urllib.urlopen(finalurl).info().getheaders("Content-Length")[0])/1024)/1024
+        # Append the filesize to the array of filesizes
+        filesizes.append(filesize)
+        # Downloads the file to the directory the user enters
+        # TODO urllib.urlretrieve(finalurl, directory+"/"+filename)
         counter += 1
-        print "Downloaded %s demos" % (counter)
+        # Tell user the current status and file information
+        print "%s demos remaining. Starting %s: %s MB." % (len(demoIDs)-counter, filename, filesize)
+    print "Total data transferred: %s. Enjoy!" % (totalData(filesizes))
+    return True
+
+
+def totalData(filesizes):
+    filesizes = filesizes
+    # Create a variable to add to when looping
+    data = 0
+    for i in range(0, len(filesizes)):
+        data += int(filesizes[i])
+    if data > 1024:
+        data = "%.2f GB" % (float(data) / 1024)
+    else:
+        data = "%s MB" % (data)
+    return data
+
+
+def getHTML(url):
+    # Get the variable passed as an argument so it can be used
+    url = url
+    # Open the URL
+    opener = urllib2.build_opener()
+    # Spoof the user agent
+    opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
+    response = opener.open(url)
+    # Read the response as HTML
+    html = response.read()
+    return html
+
+
+def printErrors(errors):
+    errors = errors
+    if len(errors) == 1:
+        print "%s match has no demo:" % (len(errors))
+        # Reset counter varibale to count the errors
+        counter = 1
+        # Loop through the array of matches with no demos.
+        for i in range(0, len(errors)):
+            # Print URLs for the matches with no demo file
+            print "%s: https://www.hltv.org/matches/%s" % (counter, errors[i])
+            counter += 1
+    elif len(errors) > 0:
+        print "%s matches have no demo:" % (len(errors))
+        # Reset counter varibale to count the errors
+        counter = 1
+        # Loop through the array of matches with no demos.
+        for i in range(0, len(errors)):
+            # Print URLs for the matches with no demo file
+            print "%s: https://www.hltv.org/matches/%s" % (counter, errors[i])
+            counter += 1
+    else:
+        print "No errors found!"
     return True
 
 
 # Calls the method for a given Event ID.
-eventID = raw_input("What is the event ID? ")
-demoIDs = getIDs(eventID)
+# TODO eventID = raw_input("What is the event ID? ")
+eventID = 2639
+matchIDs = getMatchIDs(eventID)
+demoIDs = getDemoIDs(matchIDs)
 download(demoIDs)
